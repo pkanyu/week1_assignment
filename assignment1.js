4e73589430254697272d539ca424be4fe25d4c3c
programming-king <petersonkariuki91@gmail.com>
2023-10-09 21:25:35 +0300

    Initial commit of assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,8 @@
+let firstname = "Peter Kariuki";
+let lastname = "Kanyu";
+let email = "petersonkariuki91@gmail.com";
+let number = 22;
+let iAmLearningJavascript = true;
+
+let sentence = "My name is "  + firstname + ",I am "+number+ "years old and it's " + iAmLearningJavascript +",I am learning Javascript"
+console.log(sentence) 
